810dc773c2e1b62609a282a808d90a2f6573b738
Rob <robstlezin@gmail.com>
2017-07-19 12:56:11 -0700

    subreddits and users saving in db

--- db/seeds/seeds.js
+++ db/seeds/seeds.js
@@ -10,6 +10,24 @@ let createUsers = (knex, id) => {
   });
 };
 
+let createSubreddits = (knex, id) => {
+  return knex('Subreddits').insert({
+    id,
+    title: "Bobson Thunderblood",
+    description: "does cool stuff" ,
+    upvote_threshold: 5,
+    location_threshold: 2
+  });
+};
+
+let createNotifications = (knex, id) => {
+  return knex('Notifications').insert({
+    id,
+    notification_message_id: id,
+    user_id: id,
+    seen: 'false'
+  });
+};
 
 // let createRecord = (knex, id) => {
 //   return knex('users').insert({
@@ -20,19 +38,26 @@ let createUsers = (knex, id) => {
 // }
 
 
-
-
 exports.seed = (knex, Promise) => {
   // Deletes ALL existing entries
   return knex('Users').del()
-    .then(() => {
-      let records = [];
-      for(let i = 1; i < 10; i++){
-        records.push(createUsers(knex, i));
-      }
-      return Promise.all(records);
-    }).catch((err) => {
-      console.log("err:", err);
-    });
+  .then(()=> {
+   return knex('Subreddits').del()
+  })
+  // .then(()=> {
+  //  return knex('Notifications').del()
+  // })
+  .then(() => {
+    let records = [];
+    for(let i = 1; i < 10; i++){
+      console.log(i);
+      records.push(createUsers(knex, i));
+      records.push(createSubreddits(knex, i));
+      // records.push(createNotifications(knex, i));
+    }
+    return Promise.all(records);
+  }).catch((err) => {
+    console.log("err:", err);
+  });
 
 };
