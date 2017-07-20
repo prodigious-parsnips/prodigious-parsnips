04c6584ac33353f16c6c299dd8b78a9e474b47db
Nick Akey <nick@nickakey.com>
2017-07-20 11:55:26 -0700

    User get query built

--- db/models/subreddits.js
+++ db/models/subreddits.js
@@ -3,7 +3,7 @@ const db = require('../');
 const Subreddit = db.Model.extend({
   tableName: 'Subreddits',
   users: function() {
-    return this.belongsToMany('User').through('Users_Subreddits_Prefs', null, 'user_id')
+    return this.belongsToMany('User').through('Users_Subreddits_Prefs', 'subreddit_it', 'user_id')
   },
   users_subreddits_prefs: function(){
     return this.hasMany('Users_Subreddits_Prefs');
--- db/models/users_subreddits_prefs.js
+++ db/models/users_subreddits_prefs.js
@@ -3,7 +3,7 @@ const db = require('../');
 const Users_Subreddits_Prefs = db.Model.extend({
   tableName: 'Users_Subreddits_Prefs',
   user_preferences: function() {
-    return this.hasMany('User_Preferences');
+    return this.belongsTo('User_Preferences');
   },
   users: function(){
     return this.belongsTo('User');
