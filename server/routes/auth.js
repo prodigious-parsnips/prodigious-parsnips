33cb8b46efabd7313311fbad10e25c00b2f7d7c8
Nick Akey <nick@nickakey.com>
2017-07-24 17:14:59 -0700

    server routes for login and signup complete

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -10,7 +10,7 @@ router.route('/')
 
 router.route('/login')
   .get((req, res) => {
-    res.render('login.ejs', { message: req.flash('loginMessage') });
+    res.send({'fail': 'fail'});
   })
   .post(middleware.passport.authenticate('local-login', {
     successRedirect: '/profile',
@@ -20,7 +20,8 @@ router.route('/login')
 
 router.route('/signup')
   .get((req, res) => {
-    res.render('signup.ejs', { message: req.flash('signupMessage') });
+    res.send({'fail': 'fail'});
+    //render('signup.ejs', { message: req.flash('signupMessage') });
   })
   .post(middleware.passport.authenticate('local-signup', {
     successRedirect: '/profile',
@@ -30,7 +31,7 @@ router.route('/signup')
 
 router.route('/profile')
   .get(middleware.auth.verify, (req, res) => {
-    res .send(req.user);
+    res.send(req.user);
   });
 
 router.route('/logout')
